ad696c42e4ce017b75d11e61f04d293e149b83ea
Bibek Ghimire <bibekghimire99@gmail.com>
2025-08-06 15:59:24 +0545

    add class method assignable role based on request user

--- utils/choices.py
+++ utils/choices.py
@@ -1,10 +1,24 @@
 from django.db import models
 from rest_framework.permissions import BasePermission
 
+ROLE_ASSIGNABLE = {
+    'SU':['AD','ST', 'CR'],
+    'AD': ['ST', 'CR'],
+    'ST': ['CR'],
+    'CR': []
+}
+
 class RoleChoices(models.TextChoices):
     ADMIN= 'AD', 'Admin'
     STAFF= 'ST', 'Staff'
     CREATOR= 'CR', 'Creator'
+
+    @classmethod
+    def get_assignable_roles(cls, role):
+         return [
+            {"value": r, "label": cls(r).label}
+            for r in ROLE_ASSIGNABLE.get(role, [])
+        ]
 
 class StatusChoices(models.TextChoices):
     ACTIVE='ACT', 'Active'
