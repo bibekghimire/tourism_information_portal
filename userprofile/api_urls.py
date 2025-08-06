5b3ad91d5024fb56bcf500c10cd043f67b3aefe7
Bibek Ghimire <bibekghimire99@gmail.com>
2025-08-06 15:56:12 +0545

    add url for view, get_user_roles

--- userprofile/api_urls.py
+++ userprofile/api_urls.py
@@ -2,15 +2,34 @@ from django.urls import path
 from . import api_views
 
 app_name='userprofile'
+'''/api/user/'''
 urlpatterns=[
-    path('',api_views.UserListCreateView.as_view(), name='user-list-create'),
-    path('<int:id>/details/',api_views.UserRetrieveUpdateDeleteView.as_view(),name='user-retrieve-update-delete'),
+    path('',
+         api_views.UserListCreateView.as_view(), 
+         name='user-list-create'),
+    path('<int:id>/details/',
+         api_views.UserRetrieveUpdateDeleteView.as_view(),
+         name='user-retrieve-update-delete'),
     path(
-        'change-password/',api_views.ChangePasswordView.as_view(),
+        'change-password/',
+        api_views.ChangePasswordView.as_view(),
         name='user-change-password'
     ),
     path(
-        '<int:id>/reset-password/', api_views.ResetPasswordView.as_view(),
+        'admin/<int:id>/reset-password/', 
+        api_views.ResetPasswordView.as_view(),
         name='user-reset-password',
     ),
+    path(
+        'userprofile/', 
+        api_views.UserProfileListCreateView.as_view(),
+        name='userprofile-list-create'
+    ),
+    path('userprofile/details/', 
+        api_views.SelfUserProfileDetailUpdateView.as_view(), 
+        name='self-profile-update-delete'),
+    path('admin/userprofile/<int:id>/details/',
+         api_views.SuperUserProfileDetailUpdateDeleteView.as_view(),
+         name='super-userprofile-detail-update-delete'),
+    path('role-choices/',api_views.GetRoleChoices.as_view(),name='get-role-choices')
 ]
