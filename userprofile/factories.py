a69d538b02b61b799cc04295a9ab8acbd4693a15
Bibek Ghimire <bibekghimire99@gmail.com>
2025-08-07 14:26:48 +0545

    change first_name and last_name to return pure alphabet

--- userprofile/factories.py
+++ userprofile/factories.py
@@ -3,6 +3,7 @@ from faker import Faker
 from django.contrib.auth import get_user_model
 from userprofile.models import UserProfile
 from utils import choices
+import re
 
 
 User=get_user_model()
@@ -12,16 +13,16 @@ class UserFactory(factory.django.DjangoModelFactory):
         model=User
     
     username=factory.Sequence(lambda n: f"user{n}")
-    password = factory.PostGenerationMethodCall('set_password', 'testpass')
+    password = factory.PostGenerationMethodCall('set_password', '123')
 
 class UserProfileFactory(factory.django.DjangoModelFactory):
     class Meta:
         model=UserProfile
 
     user=factory.SubFactory(UserFactory)
-    first_name=factory.LazyAttribute(lambda x: Faker().name()[:20])
+    first_name=factory.LazyAttribute(lambda x: re.sub(r'[^a-zA-Z]', '', Faker().last_name())[:20])
     phone_number = factory.Sequence(lambda n: f"98{str(10000000 + n).zfill(8)}")
-    last_name=factory.LazyAttribute(lambda x: Faker().name()[:20])
+    last_name=factory.LazyAttribute(lambda x: re.sub(r'[^a-zA-Z]', '', Faker().last_name())[:20])
     email = factory.Sequence(lambda n: f"email{n}@fact.com")
     date_of_birth="2000-01-01"
     role=factory.Iterator([choice.value for choice in choices.RoleChoices])
