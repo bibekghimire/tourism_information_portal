9e2fd0009b02152791c1da2aee04702d953cc3fc
Bibek Ghimire <bibekghimire99@gmail.com>
2025-08-11 06:48:53 +0545

    update permission

--- visitor_record/api_views.py
+++ visitor_record/api_views.py
@@ -1,7 +1,7 @@
 #api_views.py
 from . models import  Visitor, Review, Group
 from . import serializers as serializers_
-
+from rest_framework.permissions import IsAuthenticated
 from userprofile.models import UserProfile
 
 from rest_framework.generics import GenericAPIView
@@ -11,6 +11,7 @@ class GroupListCreateView(GenericAPIView,
                           mixins.ListModelMixin, 
                           mixins.CreateModelMixin
                           ):
+    permission_classes=[IsAuthenticated]
     def get_serializer_class(self):
         if self.request.method=='POST':
             return serializers_.GroupCreateSerializer
@@ -23,4 +24,45 @@ class GroupListCreateView(GenericAPIView,
     def post(self,request,*args,**kwargs):
         return self.create(request,*args,**kwargs)
     
-class 
+class GroupRetriveUpdateDeleteView(
+    GenericAPIView, mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin
+):
+    serializer_class=serializers_.GroupDetailSerializer
+    queryset=Group.objects.all()
+    lookup_field='id'
+    lookup_url_kwarg='id'
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+    def patch(self,request,*args,**kwargs):
+        return self.partial_update(request,*args,**kwargs)
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
+
+class VisitorListCreateView(GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    def get_serializer_class(self):
+        if self.request.method=='POST':
+            return serializers_.VisitorCreateSerializer
+        return serializers_.VisitorListSerializer
+    queryset=Visitor.objects.all()
+    lookup_field='id'
+    lookup_url_kwarg='id'
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+    
+class VisitorRetriveUpdateDeleteView(
+    GenericAPIView, mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin
+):
+    queryset=Visitor.objects.all()
+    serializer_class=serializers_.VisitorDetailSerializer
+    lookup_field='id'
+    lookup_url_kwarg='id'
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+    def patch(self,request,*args,**kwargs):
+        return self.partial_update(request,*args,**kwargs)
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
