c806244ca0add72ac9ee7e6c0e130c5baea06325
Bibek Ghimire <bibekghimire99@gmail.com>
2025-08-14 07:59:19 +0545

    add travel factory

--- activity/factories.py
+++ activity/factories.py
@@ -2,7 +2,8 @@ import factory
 from faker import Faker
 from userprofile.factories import UserProfileFactory, UserFactory
 import random
-from activity.models import ActivityType, Activity, Destination, Route
+from activity.models import ActivityType, Activity, Destination, Route, Travel
+from utils import choices
 
 def activity_title(n):
     return f'activity {n}'
@@ -14,6 +15,8 @@ def activity_title(n):
     return titles[r] if r>0 else titles[random.randint(0,9)]
 class BaseFactory(factory.django.DjangoModelFactory):
     created_by=factory.SubFactory(UserFactory)
+    class Meta:
+        abstract=True
 
 class ActivityTypeFactory(BaseFactory):
     class Meta:
@@ -46,8 +49,8 @@ class RouteFactory(BaseFactory):
     class Meta:
         model=Route
     title=factory.Sequence(lambda n: f'route{n}')
-    start_point=title=factory.Sequence(lambda n: f'start{n}')
-    end_point=title=factory.Sequence(lambda n: f'end{n}')
+    start_point=factory.Sequence(lambda n: f'start{n}')
+    end_point=factory.Sequence(lambda n: f'end{n}')
     @factory.post_generation
     def destinations(self,create,extracted,**kwargs):
         if not create:
@@ -56,3 +59,44 @@ class RouteFactory(BaseFactory):
             self.destinations.set(extracted)
         else:
             self.destinations.set(DestinationFactory.create_batch(2))
+
+class TravelFactory(BaseFactory):
+    
+    class Meta:
+        model=Travel
+    title=factory.Sequence(lambda n: f'travel{n}')
+    descriptions=factory.Sequence(lambda n: f'Description {n}')
+    difficulty=factory.LazyFunction(lambda : random.choice(['easy','moderate','hard']))
+    duration=factory.LazyFunction(lambda : random.randint(1,15))
+    max_altitude=factory.LazyFunction(lambda : random.randint(1000,5500))
+    min_altitude=700
+    distance=factory.LazyFunction(lambda : random.randint(1,55))
+    best_season=factory.LazyFunction(lambda : random.choice([choice.value for choice in choices.SeasonChoices]))
+    required_permits="No permits Required"
+    is_guided_only=random.choice([True, False])
+    
+    @factory.post_generation
+    def destinations(self,create,extracted,**kwargs):
+        if not create:
+            return 
+        if extracted:
+            self.destinations.set(extracted)
+        else:
+            self.destinations.set(DestinationFactory.create_batch(2))
+    @factory.post_generation
+    def activities(self,create,extracted,**kwargs):
+        if not create:
+            return 
+        if extracted:
+            self.activities.set(extracted)
+        else:
+            self.activities.set(ActivityFactory.create_batch(2))
+    @factory.post_generation
+    def routes(self,create,extracted,**kwargs):
+        if not create:
+            return 
+        if extracted:
+            self.routes.set(extracted)
+        else:
+            self.routes.set(RouteFactory.create_batch(2))
+        
